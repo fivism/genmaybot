6a8ff89fb33b1628099c0853ce290496225381a0
Dan Vu Quoc <dan@vuquoc.com>
2015-02-16 21:03:19 -0800

    Ugg 2

--- botmodules/rules.py
+++ botmodules/rules.py
@@ -74,7 +74,7 @@ rules = { 1: "Obey the Rules", 2: "Lead by example", 3: "Guide the uninitiated",
 
 def getRule(self, e):
         try:
-                if (int(e.input)) in rules:
+                if (e.input) in rules:
                         e.output = rules[int(e.input)]
                 else:
                         e.output = rules[random.randint(1,len(rules))]

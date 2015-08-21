eb5b5b9007e4a50267d94e6d4bd84c4497b7e704
Dan Vu Quoc <dan@vuquoc.com>
2015-08-21 14:55:16 -0700

    Might fix it...

--- botmodules/decider.py
+++ botmodules/decider.py
@@ -3,7 +3,7 @@ def decider(self, e):
     if "red dot" in e.input.lower():  
         self.irccontext.mode(e.source, '+b {}'.format(e.hostmask))
         self.irccontext.kick(e.source, e.nick, "Congratulations! You found the word of the day, courtesy of red dot!")
-    regex = "^bot" + "[^\s]? (.*) or ([^?]*)"
+    regex = "^!bot\s(.*?)\s+or\s+([^?\n]*)$"
     result = re.search(regex, e.input)
     if result:
         if (random.randint(0,1) == 0):
